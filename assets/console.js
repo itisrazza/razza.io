777d374e999f613f6102c995b6a323dbc0e5510a
Raresh Nistor <raresh.nistor@fluxfederation.com>
2025-04-10 11:12:42 +1200

    Add a new 404 page

---
+++ assets/console.js
@@ -0,0 +1,5 @@
+console.info(`You're more than welcome to look around here, but this whole this whole thing is on GitHub.
+
+More info: https://razza.io/projects/razza.io
+GitHub: https://github.com/itisrazza/razza.io
+`);
