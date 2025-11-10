fc3baf238fa0771ddd1a5cd415fcfa4402920ca3
Raresh Nistor <raresh@nistor.email>
2025-11-10 21:21:20 +1300

    add some more images to the gallery

--- _scripts/generate-gallery-thumbnails.sh
+++ _scripts/generate-gallery-thumbnails.sh
@@ -2,9 +2,10 @@
 
 set -xeuo pipefail
 
-THUMB_GEOMETRY="1024x1024"
+THUMB_GEOMETRY="500x500"
 
+mkdir -p assets/gallery/thumbs
 for image_path in assets/gallery/*.jpg; do
   image="$(basename "$image_path" ".jpg")"
-  convert "assets/gallery/$image.jpg" -thumbnail "$THUMB_GEOMETRY" "assets/gallery/thumbs/$image.jpg"
+  convert "assets/gallery/$image.jpg" -auto-orient -strip -thumbnail "$THUMB_GEOMETRY" "assets/gallery/thumbs/$image.jpg"
 done
